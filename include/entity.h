ddf85aab1b0ec66796f06362de43e1f99849f08e
Erik Wigert <erik.wigert123@gmail.com>
2023-01-26 08:31:08 +0100

    Change to entity type

---
+++ include/entity.h
@@ -0,0 +1,61 @@
+#ifndef entity_h
+#define entity_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+
+// This program should handle the functions regarding entities.
+// Entities can be one of the following:
+// - Player Characters
+// - Monsters
+// - Items
+
+// -------
+// Structs
+// -------
+
+typedef struct entity{
+    char name[10];
+    char type[5];
+    char behaviour[5];
+    char description[300];
+    int currentHP;
+    unsigned int maxHP;
+    int DEX;
+    int DMG;
+    int DEF;
+    // room* room;
+    int posX;
+    int posY;
+    int noItems;
+    entity** items;
+} entity;
+
+// ---------
+// Functions
+// ---------
+
+// This function initiates an entity for the current session
+// Variables to be added: pointer to current room, and int coordinates in that room.
+// Arguments are: name, Max Hit Points, Damage, Defense, Dexterity
+entity* createEntity(char[10], int, int, int, int);
+
+// This function will free the entity pointer from memory
+void freeEntity(entity*);
+
+// This function deals damage to an entity equal to the integer argument. The function then returns the current hit points of the entity.
+int damageEntity(entity*, int);
+
+// This function contains the logic to use and enter the item menu
+void commandItemMenu(entity**);
+
+// This function prints the item menu, the integer being the number of items
+void printItemMenu(entity**, int);
+
+// This function prints info about a specific item from a file
+void printEntity(entity*);
+
+#endif
---
+++ src/entity.c
@@ -0,0 +1,88 @@
+#include "entity.h"
+
+entity* createEntity(char name[10], int HP, int DMG, int DEF, int DEX)
+{
+    // Create pointer to entity in memory
+    entity* ePtr = (entity*) malloc(sizeof(struct entity));
+
+    strcpy(name, ePtr->name);
+    ePtr->maxHP = HP;
+    ePtr->currentHP = ePtr->maxHP;
+    ePtr->DMG = DMG;
+    ePtr->DEF = DEF;
+    ePtr->DEX = DEX;
+
+    strcpy(NULL, ePtr->type);
+    strcpy(NULL, ePtr->behaviour);
+    strcpy(NULL, ePtr->description);
+
+    // ePtr->room = room;
+    // ePtr->posX = x;
+    // ePtr->posY = y;
+
+    return(ePtr);
+}
+
+void freeEntity(entity* ePtr)
+{
+    free(ePtr);
+}
+
+
+int damageEntity(entity* ePtr, int damage)
+{
+    ePtr->currentHP -= damage;
+    return(ePtr->currentHP);
+}
+
+
+void commandItemMenu(entity** items) {
+    
+    int answer;
+    int noOfItems = (sizeof(items)/sizeof(entity*));
+
+    system("cls");
+    printItemMenu(items, noOfItems);
+    printf("?: ");
+    fflush(stdin);
+    scanf("%d", &answer);
+    puts("");
+
+    int i = 0;
+
+    // Have to use if-statement since we have a dynamic array
+    while (i == 0) {
+        if (answer == -1) {
+            i = 1;
+        }
+        else if (answer >= 1 && answer <= noOfItems) {
+            printItem(items[answer - 1]);
+        }
+        else {
+            puts ("\nPlease enter a valid input!\n");
+        }
+        if (i == 0) {
+            system("cls");
+            printItemMenu(items, noOfItems);
+            printf("?: ");
+            fflush(stdin);
+            scanf("%d", &answer); 
+            puts("");
+        }
+    }
+}
+
+void printItemMenu(entity** items, int num) {
+
+    printf("What item would you like to examine?");
+    for (int i = 1; i <= num; i++) {
+        printf(i + ": ");
+        printf(items[i]->name);
+        puts("");
+    }
+}
+
+void printEntity(entity* item) {
+    printf(item->description);
+    puts("");
+}
