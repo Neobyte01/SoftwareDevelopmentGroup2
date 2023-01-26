075c05fb4ace97bae43cd6f82d0e9fbc244fe1f2
Aaetpio <gcasi13579@gmail.com>
2023-01-26 09:31:07 +0100

    Revert "Readjusted menu to allow testing of combat" since it does not belong on main branch
    This reverts commit d71050bc67c472eb6154bd807052a4b1f6860f2a.

--- src/menu.c
+++ src/menu.c
@@ -1,7 +1,6 @@
 #include "menu.h"
-#include "combat.h"
 
-void commandMenu (void) 
+void commandMainMenu (void) 
 {
     
     int answer;
@@ -13,30 +12,25 @@ void commandMenu (void)
     puts("");
 
     int i = 0;
-    player p = {.ID = 3,.currentHP = 3,.maxHP = 3,.DEX = 3,.DMG = 3,.DEF = 3,.posX = 3,.posY = 3,.items = NULL,.noItems = 0};
-    monster m = {.ID = 2,.currentHP = 10,.maxHP = 10,.DEX = 2,.DMG = 2,.DEF = 2,.posX = 3,.posY = 3};
+
     while (i == 0) 
     {
         switch (answer)
         {
-        case 1:
-            // Call function to start game.
-            printWIP();
-            break;
-        case 2:
-            // Call function to select file to be loaded.
-            printWIP();
-            break;
-        case 3:
-            // Initiate combat
-            combat(&p,&m);
-            break;
-        case -1:
-            i = 1;
-            break;
-        default:
-            puts ("\nPlease enter a valid input!\n");
-            break;
+            case 1:
+                // Call function to start game.
+                printWIP();
+                break;
+            case 2:
+                // Call function to select file to be loaded.
+                printWIP();
+                break;
+            case -1:
+                i = 1;
+                break;
+            default:
+                puts ("\nPlease enter a valid input!\n");
+                break;
         }
         if (i == 0) {
             system("cls");
@@ -53,7 +47,7 @@ void commandMenu (void)
 void printMainMenu(void) 
 {
     printf("------------------------------------------\n Menu \n------------------------------------------\n");
-    printf("1: Start Game.\n2: Load Save.\n3: Initiate combat. \n-1: Quit\n------------------------------------------\n\n");
+    printf("1: Start Game.\n2: Load Save.\n-1: Quit\n------------------------------------------\n\n");
 }
 
 void exitPrint(void)
