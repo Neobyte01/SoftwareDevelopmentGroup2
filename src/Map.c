332b98eae70e0d8cc143009201c47f176d426542
Sami Noroozi <saminoorzy@gmail.com>
2023-01-31 18:04:35 +0100

    Map

---
+++ src/Map.c
@@ -0,0 +1,25 @@
+
+//#include "map.h"
+#include "Graph.h"
+
+void Map(){
+
+    struct Graph *graph = createGraph(5);
+
+    addEdge(graph, 1, 2); 
+
+    print("     []      []\n
+            [][][][][][][]\n
+              []  []  []\n
+                  []  []\n
+                    []\n")
+
+
+}
+
+
+
+int Main(){
+
+    Map();
+}
