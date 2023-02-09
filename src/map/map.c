a2a2f5e1e2fe0a8af6b7d5157415c219925fb274
Sami Noroozi <saminoorzy@gmail.com>
2023-02-10 00:00:57 +0100

    new moves

--- src/map/map.c
+++ src/map/map.c
@@ -1,19 +1,17 @@
 #include "map/map.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "entities/player.h"
+#include "map/movement.h"
 
 // ...
 static char check(int n, int roomId);
 
 const char* RoomNames[10] = {"Corridor", "Dinner hall", "Sleeping quarter", "Common room", "Terminal room"};
 
-struct Edge* newEdge(int dest) {
-	struct Edge* newNode = malloc(sizeof(struct Edge));
-	newNode->dest = dest;
-    
-	newNode->next = NULL;
-	return newNode;
-}
+
+
+
 
 struct Map* createMap(int V) {
 	struct Map* Map = (struct Map*)malloc(sizeof(struct Map)); Map->V = V;
@@ -25,51 +23,37 @@ struct Map* createMap(int V) {
 		Map->array[i].id = i;
 		Map->array[i].type = CORRIDOR;
 		Map->array[i].Objects =  (struct Object*)malloc(MAX_OBJECTS * sizeof(struct Object));
-		Map->array[i].head = NULL;
+		Map->array[i].left = NULL;
+		Map->array[i].right = NULL;
+		Map->array[i].up = NULL;
+		Map->array[i].down = NULL;
 	}
 
-	defultMap(Map);
+	defaultMap(Map);
 
 	return Map;
 }
 
-void addEdge(struct Map* Map, int src, int dest) {
-	struct Edge* check = NULL;
-	struct Edge* newNode = newEdge(dest);
 
-	if (Map->array[src].head == NULL) {
-		newNode->next = Map->array[src].head;
-		Map->array[src].head = newNode;
-	} else {
-		check = Map->array[src].head;
-		while (check->next != NULL)
-			check = check->next;
+void connect_rooms(Room *a,Room *b, enum Direction dir) {
 
-		check->next = newNode;
-	}
-
-	newNode = newEdge(src);
-	if (Map->array[dest].head == NULL) {
-		newNode->next = Map->array[dest].head;
-		Map->array[dest].head = newNode;
-	} else {
-		check = Map->array[dest].head;
-		while (check->next != NULL)
-			check = check->next;
-		
-		check->next = newNode;
-	}
-
+  if (dir == LEFT) {
+    a->left = b;
+    b->right = a;
+  } else if (dir == RIGHT) {
+    a->right = b;
+    b->left = a;
+  } else if (dir == UP) {
+    a->up = b;
+    b->down = a;
+  } else if (dir == DOWN) {
+    a->down = b;
+    b->up = a;
+  }
 }
 
-void printGraph() {
-	int v;
-	
-	for (v = 0; v < globalMap->V; ++v) {
-		connectedRooms(v);
-		printf("\n");
-	}
-}
+
+
 
 char check(int n, int roomId) {
     return roomId == n ? '*' : ' ';
@@ -78,28 +62,33 @@ char check(int n, int roomId) {
 void printMap(Entity *entity) {
     int id = entity->roomId;
 
-	printf("      [%c]      [%c]\n",check(2, id), check(9, id));
-	printf("   [%c][%c][%c][%c][%c][%c][%c]\n", check(1, id),check(3, id), check(5, id), check(6, id), check(10, id),check(11, id),check(13, id));
+	printf("      [%c]         [%c]\n",check(2, id), check(10, id));
+	printf("   [%c][%c][%c][%c][%c][%c][%c]\n", check(1, id),check(3, id), check(5, id), check(6, id), check(9,id),check(11, id),check(15, id));
 	printf("      [%c]   [%c]   [%c]\n",check(4, id),check(7, id), check(12, id));
-	printf("            [%c]   [%c]\n",check(8, id),check(14, id));
+	printf("            [%c]   [%c]\n",check(8, id),check(13, id));
+	printf("                  [%c]\n",check(13, id));
 	printf("            \n");
 }
 
-void defultMap(struct Map* Map) {
-    addEdge(Map, 1, 3);
-	addEdge(Map, 3, 2);
-    addEdge(Map, 3, 4);
-    addEdge(Map, 3, 5);
-	addEdge(Map, 5, 6);
-	addEdge(Map, 6, 7);
-	addEdge(Map, 7, 8);
+void defaultMap(struct Map* Map) {
 
-	addEdge(Map, 6, 10);
-    addEdge(Map, 10, 9);
-    addEdge(Map, 10, 11);
-    addEdge(Map, 11, 12);
-    addEdge(Map, 12, 14);
-	addEdge(Map, 11, 13);
+
+	connect_rooms(&Map->array[1],&Map->array[3],RIGHT);
+	connect_rooms(&Map->array[3],&Map->array[2],UP);
+	connect_rooms(&Map->array[3],&Map->array[4],DOWN);
+	connect_rooms(&Map->array[3],&Map->array[5],RIGHT);
+	connect_rooms(&Map->array[5],&Map->array[6],RIGHT);
+	connect_rooms(&Map->array[6],&Map->array[7],DOWN);
+	connect_rooms(&Map->array[7],&Map->array[8],DOWN);
+	connect_rooms(&Map->array[6],&Map->array[9],RIGHT);
+	connect_rooms(&Map->array[9],&Map->array[11],RIGHT);
+	connect_rooms(&Map->array[11],&Map->array[10],UP);
+	connect_rooms(&Map->array[11],&Map->array[12],DOWN);
+	connect_rooms(&Map->array[12],&Map->array[13],DOWN);
+	connect_rooms(&Map->array[13],&Map->array[14],DOWN);
+	connect_rooms(&Map->array[11],&Map->array[15],RIGHT);
+
+	
 
 	Map->array[1].type = SLEEPING_QUARTER;
 	Map->array[2].type = SLEEPING_QUARTER;
@@ -110,14 +99,5 @@ void defultMap(struct Map* Map) {
 	Map->array[9].type = TERMINAL_ROOM;
 }
 
-void connectedRooms(int id) {
-    struct Edge* pCrawl = globalMap->array[id].head;
-    printf("\nFrom room %d you can move to the following rooms:\n", id);
- 
-    while (pCrawl) {
-        printf("%d(%s)\n", pCrawl->dest,RoomNames[globalMap->array[pCrawl->dest].type]);
-        pCrawl = pCrawl->next;
-    }
- 
-    printf("\n");
-}
+
+
--- src/map/movement.c
+++ src/map/movement.c
@@ -1,5 +1,90 @@
 #include "map/movement.h"
+#include "entities/player.h"
+#include "map/map.h"
 
 void moveEntity(Entity *entity, enum Direction dir) {
-    // TODO: implement this function.
+    
+    int id = entity->roomId;
+
+    switch(dir){
+
+        case UP:
+        moveUp(id);
+        break;
+
+        case DOWN:
+        moveDown(id);
+        break;
+
+        case RIGHT:
+        moveRight(id);
+        break;
+
+        case LEFT:
+        moveLeft(id);
+        break;
+
+
+        default:
+        break;
+
+    }
+}
+
+
+
+
+
+
+void moveLeft(int id){
+
+
+	if(globalMap->array[id].left != NULL){
+		player->roomId =  globalMap->array[id].left->id;
+	}
+	else{
+		printf("Can't move to the left\n");
+	}
+
+
+}
+
+
+void moveRight(int id){
+	
+	if(globalMap->array[id].right != NULL){
+		player->roomId =  globalMap->array[id].right->id;
+	}
+	else{
+		printf("Can't move to the right\n");
+	}
+
+}
+
+
+void moveUp(int id){
+	
+	if(globalMap->array[id].up != NULL){
+		player->roomId =  globalMap->array[id].up->id;
+	}
+	else{
+		printf("Can't move to the up-side\n");
+	}
+
+
+}
+
+
+
+
+void moveDown(int id){
+
+	if(globalMap->array[id].down != NULL){
+		player->roomId =  globalMap->array[id].down->id;
+	}
+	else{
+		printf("Can't move to the down-side\n");
+	}
+
+
 }
