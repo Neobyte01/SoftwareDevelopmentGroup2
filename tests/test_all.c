dfe29bdbac685e4fa72179fa3dcbd6f896514c9d
Neobyte01 <neobyte01@hotmail.com>
2023-02-27 10:08:13 +0100

    Added unittest for map generation

--- tests/test_all.c
+++ tests/test_all.c
@@ -1,6 +1,7 @@
 #include "actions/test_action.c"
 #include "entities/test_monsters.c"
 #include "entities/test_player.c"
+#include "map/test_generation.c"
 #include "test_combat.c"
 
 int main() {    
@@ -9,5 +10,6 @@ int main() {
     testMonsterRegistry();
     testSetupPlayer();
     testCombat();
+    testGenerateMap();
     puts("Done.");
 }
