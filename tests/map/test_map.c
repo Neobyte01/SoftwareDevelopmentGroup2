d9fc101882977881e3bf5870e31cf1943761a7b2
darii99 <darina.larsen@yahoo.se>
2023-02-27 21:02:55 +0100

    fixed merge issue

--- tests/map/test_map.c
+++ tests/map/test_map.c
@@ -24,25 +24,6 @@ void testTerminalRoom() {
 	assert(strcmp(globalMap->array[9].description, "A tiny, sultry room with a turned on CRT-monitor on a metal desk.\n") == 0);
 }
 
-<<<<<<< HEAD
-=======
-void perfTestMapGen()
-{
-	printf("Tests generation of map\n");
-	clock_t start, end;
-	double total;
-	start = clock();
-	puts("Before");
-	struct Map *genMap = generateMap();
-	puts("After");
-	end = clock();
-	total = (double)(end - start)/CLOCKS_PER_SEC;
-	assert(total < 0.5);
-	printf("Elapsed time: %f\n", total);
-	free(genMap);
-}
-
->>>>>>> c1f247205230868ff387390a24483815657e2c13
 void testMap() {
   globalMap = generateMap(16);
 	testMapDescription();
