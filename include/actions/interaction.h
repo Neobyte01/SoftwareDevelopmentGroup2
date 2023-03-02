2aff83069092e0ecfef181cbdfad7938eb01be2d
Aaetpio <gcasi13579@gmail.com>
2023-03-02 11:40:16 +0100

    Added more room types & expanded documentation in interaction.h

--- include/actions/interaction.h
+++ include/actions/interaction.h
@@ -5,8 +5,9 @@
 // Interaction library
 // Systems for interacting with the environment.
 
-// Let the player search the room they are currently in
+// Let the player search the room they are currently in (Prints the room description)
 //
-//
-//
+// args:
+// - player: Player whose position is checked to determine what room description to show
+// - map: Used to fetch the currently inhabited room description
 void searchRoom(Entity *player, Map *map);
--- src/map/generation.c
+++ src/map/generation.c
@@ -9,7 +9,7 @@ static void defaultMap(struct Map* Map);
 static void populateItems(struct Map* Map);
 
 struct Map *generateMap() {
-    struct Map *map = createMap(15);
+    struct Map *map = createMap(16);
     defaultMap(map);
     populateItems(map);
     return map;
@@ -67,17 +67,17 @@ void defaultMap(struct Map* Map) {
 	Map->array[2].description = "A big and colorful room full of items.\n";
 	Map->array[3].description = "There is blood on the bed sheets. At the corner of the room there is a"ANSI_COLOR_CYAN" box."ANSI_COLOR_RESET"\n"; //possibility to find items in this room
 	Map->array[4].description = "Not much to see here except some fresh bizarre-looking footprints.\n";
-	Map->array[5].description = "A dark, eerie hall with\ncreaky floorboards.\n"; //can be heard by monsters when walking here
+	Map->array[5].description = "A dark, eerie hall with creaky floorboards.\n"; //can be heard by monsters when walking here
 	Map->array[6].description = "A large room filled with bookshelves, a couch, chairs and a table. A chair is knocked to the floor.\n"; //might find something in the bookshelves
 	Map->array[7].description = "A large room with a long table and many chairs. Leftovers and dirty dishes fill up the table. In the centre of it, something shiny can be seen.\n"; //a key can be found in this room
 	Map->array[8].description = "A rather small room with dark counters and dirty tiles and window.\n";
 	Map->array[9].description = "A tiny, sultry room with a turned on CRT-monitor on a metal desk.\n";
-	Map->array[10].description = "A small room with only a bunk-bed and a closet.\n";
-	Map->array[11].description = "A narrow, dark hall with a large crack in the floorboards.\n";
-	Map->array[12].description = "A room full of cabinets alongside the dinner table.\n";
-	Map->array[13].description = "A messy room with thrown books and other debris.\n";
+	Map->array[10].description = "A small room with only a bunk-bed and a "ANSI_COLOR_CYAN"closet"ANSI_COLOR_RESET".\n";
+	Map->array[11].description = "A narrow, dark hall with a large "ANSI_COLOR_CYAN"crack"ANSI_COLOR_RESET" in the floorboards.\n";
+	Map->array[12].description = "A room full of "ANSI_COLOR_CYAN"cabinets"ANSI_COLOR_RESET" alongside the dinner table.\n";
+	Map->array[13].description = "A messy room with thrown books and other "ANSI_COLOR_CYAN"debris"ANSI_COLOR_RESET".\n";
 	Map->array[14].description = "A white and long hall with slippery tiles.\n";
-	Map->array[15].description = "A tidy room with a huge bed and a large closet.\n";
+	Map->array[15].description = "A tidy room with a huge "ANSI_COLOR_CYAN"bed"ANSI_COLOR_RESET" and a large "ANSI_COLOR_CYAN"closet"ANSI_COLOR_RESET".\n";
 
     Map->array[3].Objects[0].name = "bed";
 }
